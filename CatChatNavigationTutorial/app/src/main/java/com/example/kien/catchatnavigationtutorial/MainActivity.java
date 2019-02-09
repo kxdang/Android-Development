cc08303aee7307d16f1bf0cf16665430608deef3
kxdang <ikien.dang@gmail.com>
2019-02-09 15:44:24 -0500

    Adding hamburger menu and toggle display fragments to function

--- CatChatNavigationTutorial/app/src/main/java/com/example/kien/catchatnavigationtutorial/MainActivity.java
+++ CatChatNavigationTutorial/app/src/main/java/com/example/kien/catchatnavigationtutorial/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.kien.catchatnavigationtutorial;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +25,64 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         Fragment fragment = new InboxFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.content_frame, fragment);
         ft.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch (id) {
+            case R.id.nav_drafts:
+                fragment = new DraftsFragment();
+                break;
+
+            case R.id.nav_sent:
+                fragment = new SentItemsFragment();
+                break;
+
+            case R.id.nav_trash:
+                fragment = new TrashFragment();
+                break;
+
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+
+            case R.id.nav_feedback:
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+
+            default:
+                fragment = new InboxFragment();
+
+        }
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
