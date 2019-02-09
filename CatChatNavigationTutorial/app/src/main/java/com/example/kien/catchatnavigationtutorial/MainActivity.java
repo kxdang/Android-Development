3a035c7397ae4bcdb7efc6fbcb18d635d48d0791
kxdang <ikien.dang@gmail.com>
2019-02-09 15:08:13 -0500

    Android Dev p.606 functional navigationbar with AppCompat fix

--- CatChatNavigationTutorial/app/src/main/java/com/example/kien/catchatnavigationtutorial/MainActivity.java
+++ CatChatNavigationTutorial/app/src/main/java/com/example/kien/catchatnavigationtutorial/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kien.catchatnavigationtutorial;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
     }
 }
