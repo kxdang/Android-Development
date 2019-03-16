05d746d112e444d517d9ac2e452197c75a2aeba7
kxdang <ikien.dang@gmail.com>
2019-03-16 09:58:43 -0400

    adding Starbuzz database to hold drinks using the SQLiteOpenHelper

---
+++ Starbuzz/app/src/main/java/com/example/kien/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.example.kien.starbuzz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz"; //the name of our database
+    private static final int DB_VERSION = 1; //the version of our database
+
+    StarbuzzDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+}
